6d3ab565aea07687b94b08408bf426e0501bff1b
혀느현스 <hyuns@hyuns.dev>
2022-05-20 11:39:53 +0900

    .

---
+++ src/class/index.ts
@@ -0,0 +1,5 @@
+import { OriginalClass } from "./originalClass";
+
+export function test() {
+    console.dir(OriginalClass)
+}
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import { test as ClassTest } from './class'
+
+;(() => {
+    ClassTest()
+})()
